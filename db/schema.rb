48cb6307f3ea9979fff30ddb694610e7fe076e1a
Craig Clayton <craig@thedev.me>
2015-12-07 21:00:11 -0500

    update turbo links

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207114944) do
+ActiveRecord::Schema.define(version: 20151208015431) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.text     "description"
+    t.string   "sku"
+    t.decimal  "price"
+    t.string   "event_image"
+    t.string   "event_thumb_image"
+    t.string   "details"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
