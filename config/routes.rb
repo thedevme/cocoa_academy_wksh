0f5ba442a2b4997a6b1063b0362ffe49b302cd1a
Craig Clayton <craig@thedev.me>
2017-03-13 16:47:14 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show]
   resources :charges
   resources :leads
   resources :purchases, only: [:show]
